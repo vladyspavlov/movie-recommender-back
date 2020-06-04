6b1d44673ca69810be614a9288143b5812b8bc43
Vladislav Pavlov <vladpavlov9957@gmail.com>
2020-06-04 19:21:59 +0300

    updated user controller

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -3,6 +3,7 @@ import { Container } from 'typedi'
 import * as winston from 'winston'
 import { UserService } from '../services/app/user'
 import { UserPayload } from '../interfaces/User'
+import { Types } from 'mongoose'
 
 export async function deleteUser(req: Request, res: Response, next: NextFunction) {
     const logger: winston.Logger = Container.get('AppLogger')
@@ -17,6 +18,29 @@ export async function deleteUser(req: Request, res: Response, next: NextFunction
         await userServiceInstance.deleteUser(payload.id)
 
         return res.sendStatus(204)
+    } catch (e) {
+        logger.error('🔥 error ', e)
+        return next(e)
+    }
+}
+
+export async function addSeenMovie(req: Request, res: Response, next: NextFunction) {
+    const logger: winston.Logger = Container.get('AppLogger')
+    const userServiceInstance = Container.get(UserService)
+    const payload = res.locals['payload'] as UserPayload
+
+    try {
+        const userId = payload.id
+        const mediaId = Types.ObjectId(req.body['media'])
+        const score = Number(req.body['score'])
+        const seen = await userServiceInstance.addSeenMovie(userId, mediaId, score)
+
+        res.locals['status'] = 200
+        res.locals['response'] = {
+            seen
+        }
+
+        return next()
     } catch (e) {
         logger.error('🔥 error ', e)
         return next(e)
